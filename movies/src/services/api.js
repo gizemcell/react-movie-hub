a2adbbcbab5ebc3b56e95c9b695658bb5d654707
Gizem Celik <gzmck237884@gmail.com>
2025-03-05 23:54:21 +0300

    fixed fetch problems

--- movies/src/services/api.js
+++ movies/src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY=process.env.REACT_APP_API_KEY;
+const API_KEY=import.meta.env.VITE_API_KEY;
 const BASE_URL="https://api.themoviedb.org/3";
 
 export const getPopularMovies= async ()=>{
@@ -8,7 +8,7 @@ export const getPopularMovies= async ()=>{
 }
 
 export const searchMovies= async (query)=>{
-    const response=await fetch(`${BASE_URL}/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const response=await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
     const data=await response.json();
     return data.results;
 
